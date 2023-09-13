b1ae61fda73dd61f03c04a47c662082b2182e52f
Summit2 <pinkslime987@gmail.com>
2023-09-14 00:47:59 +0300

    lab1 not complete

---
+++ bmstu_lab_m/views.py
@@ -0,0 +1,39 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from datetime import date 
+
+'''Заявки на доставку грузов на Марс на Starship. 
+Услуги - товары, доставляемыe на Марс на Starship, 
+   заявки - заявки на конкретный объем товаров
+'''
+# def hello(request):
+#     
+#     return render(request, 'index.html', {
+#         'data' :    {'current_date': date.today(),
+#                      'list' : [1,2,3]
+#                      }
+
+        
+#     })
+
+
+    # кроме html видимо можно передавать еще и типа json (или словарей)
+def GetOrders(request):
+    return render(
+        request,'orders.html', {'data' : {
+        'current_date': date.today(),
+        'orders': [
+            {'title': 'Книга с картинками', 'id': 1},
+            {'title': 'Бутылка с водой', 'id': 2},
+            {'title': 'Коврик для мышки', 'id': 3},
+        ]
+    }}
+    )
+
+def GetOrder(request, id):
+    return render(request, 'order.html', {'data' : {
+        'current_date': date.today(),
+        'id': id
+    }})
