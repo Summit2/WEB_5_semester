388f978d3cd4cd9ddb3514dd1a6b0578d185cf88
Summit2 <pinkslime987@gmail.com>
2023-10-09 17:24:07 +0300

    фильтрация через ORM добавлена

--- bmstu_lab_m/serializers.py
+++ bmstu_lab_m/serializers.py
@@ -1,10 +1,10 @@
-from models import Cargo
+from .models import Cargo
 from rest_framework import serializers
 #  для преобразования в json и обратно
 
-class StockSerializer(serializers.ModelSerializer):
+class CargoSerializer(serializers.ModelSerializer):
     class Meta:
         # Модель, которую мы сериализуем
         model = Cargo
         # Поля, которые мы сериализуем
-        fields = ["pk", "company_name", "price", "is_growing", "date_modified"]
+        fields = ["pk", "title", "image_url", "weight", "description", 'is_deleted']
--- bmstu_lab_m/views.py
+++ bmstu_lab_m/views.py
@@ -1,15 +1,27 @@
 
-
+# django methods
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+
+# PostgreSQL
 import psycopg2
 
+# ORMs
 from bmstu_lab_m.models import Cargo
 from bmstu_lab_m.models import CargoOrder
 #from bmstu_lab_m.models import DeliveryOrders
 #from bmstu_lab_m.models import Users
 
+# все для Rest Api
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+# serializers
+from bmstu_lab_m.serializers import CargoSerializer
 
 
 '''Заявки на доставку грузов на Марс на Starship. 
@@ -23,21 +35,21 @@ def GetAllCargo(request):
     
     res=[]
     input_text = request.GET.get("good_item")
-    data = Cargo.objects.filter(is_deleted=False).order_by('weight')
+    data = Cargo.objects.filter(is_deleted=False)
     
     if input_text is not None:
-        for elem in data:
+        # for elem in data:
         
-            if input_text in elem.title:
-                res.append(elem)
-                #print(elem)
+        #     if input_text in elem.title:
+        #         res.append(elem)
+        #         #print(elem)
+
         return render(
         request,'all_cargo.html', {'data' : {
-            'items' : res,
+            'items' : Cargo.objects.filter(is_deleted=False, title__contains=input_text),
             'input' : input_text
         } }
                      )
-    
     
     return render(
             request,'all_cargo.html', {
@@ -52,18 +64,11 @@ def GetAllCargo(request):
 def GetCurrentCargo(request, id):
     data = Cargo.objects.filter(id_cargo=id)
     
-    # result ={}
-    # for i in arr:
-    #     if i['id'] == id:
-    #         result = i
     return render(request, 'current_cargo.html', 
         {'data' : {
         'item' : data[0]
     }}
     )
-
-
-from django.urls import reverse
 
 @csrf_exempt
 def DeleteCurrentCargo(request):
@@ -83,3 +88,55 @@ def DeleteCurrentCargo(request):
         return HttpResponseRedirect(redirect_url)
     
 
+@api_view(['Get'])
+def get_list(request, format=None):
+    """
+    Возвращает список грузов
+    """
+    print('get')
+    cargo = Cargo.objects.all()
+    serializer = CargoSerializer(cargo, many=True)
+    return Response(serializer.data)
+
+@api_view(['Post'])
+def post_list(request, format=None):    
+    """
+    Добавляет новый груз
+    """
+    print('post')
+    serializer = CargoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['Get'])
+def get_detail(request, pk, format=None):
+    stock = get_object_or_404(Cargo, pk=pk)
+    if request.method == 'GET':
+        """
+        Возвращает информацию о грузе
+        """
+        serializer = CargoSerializer(stock)
+        return Response(serializer.data)
+
+@api_view(['Put'])
+def put_detail(request, pk, format=None):
+    """
+    Обновляет информацию о грузе
+    """
+    stock = get_object_or_404(Cargo, pk=pk)
+    serializer = CargoSerializer(stock, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['Delete'])
+def delete_detail(request, pk, format=None):    
+    """
+    Удаляет информацию о грузе
+    """
+    stock = get_object_or_404(Cargo, pk=pk)
+    stock.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
