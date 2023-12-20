537497773713fcdb799454f3d464f2b9cb494514
Summit2 <pinkslime987@gmail.com>
2023-12-20 17:27:19 +0300

    add cors

--- bmstu_lab/settings.py
+++ bmstu_lab/settings.py
@@ -62,8 +62,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+       "https://Summit2.github.io"  ,
+]
 ROOT_URLCONF = 'bmstu_lab.urls'
 
 TEMPLATES = [
